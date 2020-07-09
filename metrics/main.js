6ccb49c19d5971288ee6fb1b7e9b6240058c5bc2
Jasper van Riet <jaspervr@30mhz.com>
2020-07-09 19:25:31 +0200

    Clean up code

--- metrics/main.js
+++ metrics/main.js
@@ -7,13 +7,13 @@ const lighthouse = require('lighthouse');
 const minimist = require('minimist');
 const fs = require('fs');
 const chromeHarCapturer = require('chrome-har-capturer');
-const percentile = require("percentile");
+const percentile = require('percentile');
 
 const PORT = 8041;
-const FOLDER = '../results/'
-const FILE = 'result.csv'
+const FOLDER = '../results/';
+const FILE = 'result.csv';
 
-const OPTS = { 
+const OPTS = {
   port: PORT,
   disableStorageReset: true,
   emulatedFormFactor: 'desktop',
@@ -27,10 +27,12 @@ function avg(list) {
   if (list.length == 0) {
     return 0;
   }
-  var total = 0;
+
+  let total = 0;
   for (let i = 0; i < list.length; i++) {
     total += list[i];
   }
+
   return total / list.length;
 }
 
@@ -41,18 +43,19 @@ function median(list) {
 
   const half = Math.floor(list.length / 2);
 
-  list.sort(function(a,b){
-    return a-b;
+  list.sort(function (a, b) {
+    return a - b;
   });
 
   if (list.length % 2) {
     return list[half];
   }
+
   return (list[half - 1] + list[half]) / 2.0;
 }
 
 async function runLighthouse(url) {
-  return lighthouse(url, OPTS, null).then(results => {
+  return lighthouse(url, OPTS, null).then((results) => {
     // use results.lhr for the JS-consumeable output
     // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
     // use results.report for the HTML/JSON/CSV output as a string
@@ -62,11 +65,11 @@ async function runLighthouse(url) {
 }
 
 /**
- * Retrieves the time to widget with the lowest timestamp 
- * @param {*} userTimings 
+ * Retrieves the time to widget with the lowest timestamp
+ * @param {*} userTimings
  */
 function getLowestTimeToWidget(userTimings) {
-  var lowestTime = -1;
+  let lowestTime = -1;
   const items = userTimings['items'];
   for (item in items) {
     if (items[item]['name'] === 'time-to-widget') {
@@ -79,8 +82,8 @@ function getLowestTimeToWidget(userTimings) {
   return lowestTime;
 }
 
-function getMedianTimeToWidget (userTimings) {
-  var times = [];
+function getMedianTimeToWidget(userTimings) {
+  let times = [];
   const items = userTimings['items'];
   for (item in items) {
     if (items[item]['name'] === 'time-to-widget') {
@@ -89,50 +92,49 @@ function getMedianTimeToWidget (userTimings) {
     }
   }
   return median(times);
-
 }
 
 /**
  * Runs Google Lighthouse on the given url and writes metrics to the given
  * file stream
- * 
- * @param {*} url 
+ *
+ * @param {*} url
  * @param {*} writeStream file stream of the file to write to
  */
 async function doTest(url, writeStream) {
-  const results = await runLighthouse(url)
-  const rawData = results.lhr
-  const userTimings = rawData.audits['user-timings']['details']
+  const results = await runLighthouse(url);
+  const rawData = results.lhr;
+  const userTimings = rawData.audits['user-timings']['details'];
 
   const logs = results.artifacts['devtoolsLogs']['defaultPass'];
   const harResult = await chromeHarCapturer.fromLog(url, logs);
   const harEntries = harResult['log']['entries'];
 
-  var totaltransfersize = 0;
-  var transfersizes = [];
-  var times = [];
+  let totaltransfersize = 0;
+  let transfersizes = [];
+  let times = [];
   for (entry in harEntries) {
-    var time = 0;
-    var request = harEntries[entry];
-    var response = request['response'];
-    var transfersize = response['_transferSize'];
+    let time = 0;
+    const request = harEntries[entry];
+    const response = request['response'];
+    const transfersize = response['_transferSize'];
 
-    var timings = request['timings']
+    const timings = request['timings'];
 
     // add up all the timing components to get the request time
     for (key in request['timings']) {
-        // null values are -1, we do not want those
-        if (timings[key] >= 0) {
-          time += timings[key]
-        }
+      // null values are -1, we do not want those
+      if (timings[key] >= 0) {
+        time += timings[key];
+      }
     }
 
-    times.push(time)
-    totaltransfersize += transfersize
-    transfersizes.push(transfersize)
+    times.push(time);
+    totaltransfersize += transfersize;
+    transfersizes.push(transfersize);
   }
 
-  var metrics = [
+  const metrics = [
     rawData.audits['first-contentful-paint'].numericValue,
     rawData.audits['first-meaningful-paint'].numericValue,
     rawData.audits['speed-index'].numericValue,
@@ -160,11 +162,10 @@ async function doTest(url, writeStream) {
     getMedianTimeToWidget(userTimings),
   ];
 
-  var line = metrics.join(',');
+  let line = metrics.join(',');
   line += '\n';
-  
-  writeStream.write(line);
 
+  writeStream.write(line);
 }
 
 function createFileName(file) {
@@ -173,7 +174,7 @@ function createFileName(file) {
   }
 
   try {
-    var i = 2;
+    let i = 2;
     while (fs.existsSync(FOLDER + file)) {
       console.log('' + file + ' already exists! Appending ' + i);
 
@@ -183,14 +184,16 @@ function createFileName(file) {
 
       i++;
     }
-  } catch(err) {
+  } catch (err) {
     console.error(err);
   }
   return FOLDER + file;
 }
 
 function help() {
-  console.error('How to use this script:\n\n--url: the url to test\n--file: where to save the csv. If not provided, result.csv is used. All files are saved to ../results\n--runs: The number of runs to perform');
+  console.error(
+    'How to use this script:\n\n--url: the url to test\n--file: where to save the csv. If not provided, result.csv is used. All files are saved to ../results\n--runs: The number of runs to perform'
+  );
   process.exit(1);
 }
 
@@ -201,16 +204,16 @@ async function main() {
     help();
   }
 
-  var runs = args.runs;
+  let runs = args.runs;
   if (args.runs == null) {
     runs = RUNS;
   }
-    
-  var file = createFileName(args.file);
-  const writeStream = fs.createWriteStream(file, {flags: 'a'});
 
-  const headers = 
-      'first-contentful-paint,first-meaningful-paint,speed-index,total-blocking-time,estimated-input-latency,time-to-first-byte,first-cpu-idle,time-to-interactive,network-rtt,network-requests,dom-size,total-transfer-size,average-transfer-size,median-transfer-size,90th-percentile-transfer-size,95th-percentile-transfer-size,99th-percentile-transfer-size,average-transfer-time,median-transfer-time,90th-percentile-transfer-time,95th-percentile-transfer-time,99th-percentile-transfer-time,number-api-calls,lowest-time-to-widget,median-time-to-widget\n';
+  const file = createFileName(args.file);
+  const writeStream = fs.createWriteStream(file, { flags: 'a' });
+
+  const headers =
+    'first-contentful-paint,first-meaningful-paint,speed-index,total-blocking-time,estimated-input-latency,time-to-first-byte,first-cpu-idle,time-to-interactive,network-rtt,network-requests,dom-size,total-transfer-size,average-transfer-size,median-transfer-size,90th-percentile-transfer-size,95th-percentile-transfer-size,99th-percentile-transfer-size,average-transfer-time,median-transfer-time,90th-percentile-transfer-time,95th-percentile-transfer-time,99th-percentile-transfer-time,number-api-calls,lowest-time-to-widget,median-time-to-widget\n';
 
   writeStream.write(headers);
 
@@ -220,16 +223,15 @@ async function main() {
 
   for (let i = 0; i < runs; i++) {
     console.log('Running test ' + (i + 1));
-    await doTest(args.url, writeStream).catch(e => console.log(e));
+    await doTest(args.url, writeStream).catch((e) => console.log(e));
   }
 
   writeStream.close();
   console.log('Successfully written to file: ' + file);
 
   process.exit();
-
 }
 
 if (require.main === module) {
-    main();
+  main();
 }
